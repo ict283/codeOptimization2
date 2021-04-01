7659a40da8343388a4f37ba0ae1131acc006af3f
ict283 <glennlum@icloud.com>
2021-04-01 19:51:20 +0800

    Start code review

--- codeOptimization2/main.cpp
+++ codeOptimization2/main.cpp
@@ -1,4 +1,4 @@
-// For review purposes only! Does not represent the actual solution!!
+// For review purposes only! Does not represent the actual solution!!!
 
 #include <iostream>
 #include "toDoList.h"
